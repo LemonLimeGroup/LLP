6ba72db33bdc6decaa576008bcd31d1bb2756ec0
Sally Kim <Sally@lawn-143-215-127-46.lawn.gatech.edu>
2016-11-13 00:18:58 -0500

    variables

--- src/LLP_Socket.java
+++ src/LLP_Socket.java
@@ -1,9 +1,15 @@
+import java.net.SocketAddress;
+
 /**
  * Created by Sally on 11/12/16.
  */
 public class LLP_Socket {
-    byte[] sender_client;
-    byte[] receiver_client;
-    byte[] sender_server;
-    byte[] receiver_server;
+    private LLP_Packet[] sender_client;
+    private LLP_Packet[] receiver_client;
+    private LLP_Packet[] sender_server;
+    private LLP_Packet[] receiver_server;
+
+    public void connect(SocketAddress address) {
+        
+    }
 }
