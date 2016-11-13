8cdbb39a8b14f5aac9610486573b70830cd8c7e0
Sally Kim <Sally@lawn-143-215-127-46.lawn.gatech.edu>
2016-11-12 23:44:21 -0500

    socket

--- src/LLP_client.java
+++ src/LLP_client.java
@@ -2,4 +2,12 @@
  * Created by Sally on 11/12/16.
  */
 public class LLP_client {
+    connect(){
+        send syn
+                receive syn/ack
+                send ack
+    }
+    sendsomething() {
+
+    }
 }
--- src/LLP_server.java
+++ src/LLP_server.java
@@ -2,4 +2,5 @@
  * Created by Sally on 11/9/16.
  */
 public class LLP_server {
+
 }
