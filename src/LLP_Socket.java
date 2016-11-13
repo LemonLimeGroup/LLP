a923af40b08cc524d8b6d24bdd4258495eaf5507
yavila <yjas3@gatech.edu>
2016-11-13 01:29:59 -0500

    Add basic structure to this class

--- src/LLP_Socket.java
+++ src/LLP_Socket.java
@@ -1,15 +1,60 @@
-import java.net.SocketAddress;
+import java.io.IOException;
+import java.net.*;
+import java.io.Closeable;
 
 /**
  * Created by Sally on 11/12/16.
  */
 public class LLP_Socket {
-    private LLP_Packet[] sender_client;
-    private LLP_Packet[] receiver_client;
-    private LLP_Packet[] sender_server;
-    private LLP_Packet[] receiver_server;
+    private static final int MAX_WINDOW_SIZE = 1024;
+    private LLP_Packet[] send_buffer;
+    private LLP_Packet[] receive_buffer;
+    private int windowSize;
+    private DatagramSocket socket;
 
-    public void connect(SocketAddress address) {
-        
+
+    public LLP_Socket(String type) throws IllegalArgumentException {
+        try {
+            socket = new DatagramSocket();
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        send_buffer = new LLP_Packet[MAX_WINDOW_SIZE];
+        receive_buffer = new LLP_Packet[MAX_WINDOW_SIZE];
+        windowSize = 50; // default window size, unless initialized by the application
+    }
+
+    public void connect(InetAddress address, int port) {
+
+    }
+
+    public LLP_Socket accept() {
+        // not in udp, we must implement
+        return null;
+    }
+
+    public void bind(SocketAddress address) {
+
+    }
+
+    public void close() {
+
+    }
+
+    public boolean isBufferFull() {
+        return false;
+    }
+
+    public void receive(LLP_Packet packet) {
+
+
+    }
+
+    public void send(LLP_Packet packet) {
+
+    }
+
+    public void setWindowSize(int windowSize) {
+        this.windowSize = windowSize;
     }
 }
